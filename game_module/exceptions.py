1a3a1331275b46ec910c821449bc1cd3381ec224
przemekbarczyk <przemek.barczyk@gmail.com>
2020-05-31 16:13:00 +0200

    Impementacja wymaganych konstrukcji v0.3

--- game_module/exceptions.py
+++
@@ -1,7 +0,0 @@
-"""Moduł zawierający klasy wyjątków"""
-
-
-class Exception(Exception):
-    """Klasa główna wyjątku z której będą dziedziczyły wszystkie pozostałe"""
-    def __init__(self, value):
-        self.value = value
